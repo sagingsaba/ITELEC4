dc451707e2ed947e4aab03af124eae6853350c69
Neil Alfred Lapuz <lapuzneilalfredbusiness@gmail.com>
2024-04-15 05:01:12 +0800

    size change

--- include/js/javascript.js
+++ include/js/javascript.js
@@ -17,19 +17,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// eye toggle
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
-
-        function togglePassword() {
-            var passwordField = document.getElementById("password");
-            var eyeIcon = document.getElementById("eye-icon");
-            if (passwordField.type === "password") {
-                passwordField.type = "text";
-                eyeIcon.classList.remove("fa-eye");
-                eyeIcon.classList.add("fa-eye-slash");
-            } else {
-                passwordField.type = "password";
-                eyeIcon.classList.remove("fa-eye-slash");
-                eyeIcon.classList.add("fa-eye");
-            }
-        }
